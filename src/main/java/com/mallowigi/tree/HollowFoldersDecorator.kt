439dab74483f592712faa7ab6c6618050749c1bb
mallowigi <heliosaian@gmail.com>
2020-05-23 23:49:02 +0300

    wip special folders

---
+++ src/main/java/com/mallowigi/tree/HollowFoldersDecorator.kt
@@ -0,0 +1,122 @@
+/*
+ * The MIT License (MIT)
+ *
+ *  Copyright (c) 2020 Elior "Mallowigi" Boukhobza
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+package com.mallowigi.tree
+
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.projectView.ProjectViewNode
+import com.intellij.ide.projectView.ProjectViewNodeDecorator
+import com.intellij.ide.projectView.impl.ProjectRootsUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.packageDependencies.ui.PackageDependenciesNode
+import com.intellij.ui.ColoredTreeCellRenderer
+import com.intellij.util.PlatformIcons
+import com.mallowigi.config.AtomFileIconsConfig.Companion.instance
+import com.mallowigi.icons.special.DirIcon
+import icons.MTIcons
+import java.util.*
+import javax.swing.Icon
+
+class HollowFoldersDecorator : ProjectViewNodeDecorator {
+  private val useHollowFolders: Boolean
+  private var manager: FileEditorManagerEx? = null
+
+  override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
+    val file = node.virtualFile
+    val project = node.project
+
+    // Color file status
+    if (project != null && file != null && !Disposer.isDisposed(project)) {
+      if (!useHollowFolders || !file.isDirectory) {
+        return
+      }
+      if (manager == null) {
+        manager = FileEditorManagerEx.getInstanceEx(project)
+      }
+      ApplicationManager.getApplication().invokeLater {
+        val openFiles = Arrays.asList(*manager!!.openFiles)
+        openFiles.stream()
+          .filter { virtualFile: VirtualFile ->
+            virtualFile.path.contains(file.path)
+          }
+          .findFirst()
+          .ifPresent { virtualFile: VirtualFile -> setOpenDirectoryIcon(data, virtualFile, project) }
+      }
+    }
+  }
+
+  override fun decorate(node: PackageDependenciesNode, cellRenderer: ColoredTreeCellRenderer) {}
+
+  companion object {
+    @Volatile
+    private var directory: Icon? = MTIcons.Nodes2.FolderOpen
+    private fun setOpenDirectoryIcon(data: PresentationData,
+                                     file: VirtualFile,
+                                     project: Project) {
+      try {
+        if (data.getIcon(true) is DirIcon) {
+          val openedIcon: Icon = (Objects.requireNonNull(data.getIcon(true)) as DirIcon).openedIcon
+          data.setIcon(DirIcon(openedIcon))
+        }
+        else if (ProjectRootManager.getInstance(project).fileIndex.isExcluded(file)) {
+          data.setIcon(MTIcons.EXCLUDED)
+        }
+        else if (ProjectRootsUtil.isModuleContentRoot(file, project)) {
+          data.setIcon(MTIcons.MODULE)
+        }
+        else if (ProjectRootsUtil.isInSource(file, project)) {
+          data.setIcon(MTIcons.SOURCE)
+        }
+        else if (ProjectRootsUtil.isInTestSource(file, project)) {
+          data.setIcon(MTIcons.TEST)
+        }
+        else if (data.getIcon(false) == PlatformIcons.PACKAGE_ICON) {
+          //      Looks like an open directory anyway
+          data.setIcon(PlatformIcons.PACKAGE_ICON)
+        }
+        else {
+          data.setIcon(directoryIcon)
+        }
+      }
+      catch (e: Exception) {
+      }
+    }
+
+    private val directoryIcon: Icon?
+      private get() {
+        if (directory == null) {
+          directory = MTIcons.Nodes2.FolderOpen
+        }
+        return directory
+      }
+  }
+
+  init {
+    useHollowFolders = instance.isUseHollowFolders
+  }
+}
