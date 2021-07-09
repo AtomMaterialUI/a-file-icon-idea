53447c6a9f6ce9c71663a25f86a5ffd19be45ca8
Elior <heliosaian@gmail.com>
2021-07-09 17:02:50 +0300

    screw kotlin seriously

---
+++ src/main/java/com/mallowigi/actions/IconToggleAction.java
@@ -0,0 +1,94 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2021 Elior "Mallowigi" Boukhobza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
+package com.mallowigi.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.actionSystem.Toggleable;
+import com.intellij.ui.LayeredIcon;
+import com.intellij.util.IconUtil;
+import com.intellij.util.ObjectUtils;
+import com.intellij.util.ui.GraphicsUtil;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class IconToggleAction extends ToggleAction {
+  @NotNull
+  private static Icon regularIcon(final Icon icon) {
+    return IconUtil.toSize(icon, JBUI.scale(18), JBUI.scale(18));
+  }
+
+  private static Icon selectedFallbackIcon(final Icon icon) {
+    return new Icon() {
+      @Override
+      public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
+        final Graphics g2d = g.create();
+
+        try {
+          GraphicsUtil.setupAAPainting(g2d);
+          g2d.setColor(JBUI.CurrentTheme.ActionButton.pressedBackground());
+          g2d.fillRoundRect(0, 0, getIconWidth(), getIconHeight(), 4, 4);
+        } finally {
+          g2d.dispose();
+        }
+      }
+
+      @Override
+      public int getIconWidth() {
+        return icon != null ? icon.getIconWidth() : JBUI.scale(18);
+      }
+
+      @Override
+      public int getIconHeight() {
+        return icon != null ? icon.getIconHeight() : JBUI.scale(18);
+      }
+    };
+  }
+
+  @Override
+  public void update(@NotNull final AnActionEvent e) {
+    final boolean selected = isSelected(e);
+    final Presentation presentation = e.getPresentation();
+    final Icon icon = presentation.getIcon();
+    Toggleable.setSelected(presentation, selected);
+
+    final Icon fallbackIcon = selectedFallbackIcon(icon);
+    final Icon actionButtonIcon = ObjectUtils.notNull(UIManager.getIcon("ActionButton.backgroundIcon"), fallbackIcon);
+
+    // Recreate the action button look
+    if (selected) {
+      e.getPresentation().setIcon(new LayeredIcon(actionButtonIcon, regularIcon(icon)));
+    } else {
+      e.getPresentation().setIcon(regularIcon(icon));
+    }
+  }
+
+}
