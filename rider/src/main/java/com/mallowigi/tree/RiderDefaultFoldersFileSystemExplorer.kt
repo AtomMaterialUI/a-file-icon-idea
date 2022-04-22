2005bce41f49c6eb0de4b4c53e7cda5288d16bc0
Elior <heliosaian@gmail.com>
2022-04-22 18:31:31 +0300

    add support for rider file system

--- rider/src/main/java/com/mallowigi/tree/RiderDefaultFoldersFileSystemExplorer.kt
+++ rider/src/main/java/com/mallowigi/tree/RiderDefaultFoldersFileSystemExplorer.kt
@@ -1,27 +1,25 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2022 Elior "Mallowigi" Boukhobza
+ *  Copyright (c) 2015-2022 Elior "Mallowigi" Boukhobza
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 package com.mallowigi.tree
 
@@ -29,36 +27,34 @@ import com.intellij.ide.projectView.PresentationData
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.ui.EmptyIcon
-import com.jetbrains.rider.projectView.views.solutionExplorer.SolutionExplorerCustomization
-import com.jetbrains.rider.projectView.workspace.ProjectModelEntity
-import com.jetbrains.rider.projectView.workspace.getVirtualFileAsParent
-import com.jetbrains.rider.projectView.workspace.isDirectory
+import com.jetbrains.rider.projectView.views.FileSystemNodeBase
+import com.jetbrains.rider.projectView.views.fileSystemExplorer.FileSystemExplorerCustomization
 import com.mallowigi.config.AtomFileIconsConfig
 import com.mallowigi.config.select.AtomSelectConfig
 import com.mallowigi.models.VirtualFileInfo
 import icons.AtomIcons
 
 /**
- * Rider custom folders decorator
+ * Rider Custom folders decorator.
  *
  * @constructor
  * @param project
  */
-class RiderDefaultFoldersDecorator(project: Project) : SolutionExplorerCustomization(project) {
+class RiderDefaultFoldersFileSystemExplorer(project: Project) : FileSystemExplorerCustomization(project) {
   /** Update node icon. */
-  override fun updateNode(presentation: PresentationData, entity: ProjectModelEntity) {
-    super.updateNode(presentation, entity)
-    val virtualFile = entity.getVirtualFileAsParent()
-
+  override fun updateNode(
+    presentation: PresentationData,
+    virtualFile: VirtualFile,
+    node: FileSystemNodeBase,
+  ) {
+    super.updateNode(presentation, virtualFile, node)
     if (!project.isDisposed) {
       when {
-        virtualFile == null                                -> return
-        !entity.isDirectory()                              -> return
+        !virtualFile.isDirectory                           -> return
         AtomFileIconsConfig.instance.isHideFolderIcons     -> hideIcon(presentation)
         !AtomFileIconsConfig.instance.isEnabledDirectories -> return
         else                                               -> matchAssociation(virtualFile, presentation)
       }
-
     }
   }
 
@@ -90,5 +86,4 @@ class RiderDefaultFoldersDecorator(project: Project) : SolutionExplorerCustomiza
    *   message.
    */
   private fun hideIcon(data: PresentationData) = data.setIcon(EmptyIcon.ICON_0)
-
 }
