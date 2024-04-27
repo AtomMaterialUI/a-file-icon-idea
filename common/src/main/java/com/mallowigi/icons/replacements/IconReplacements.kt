36394e5314e0541b1ecd85b91f0e024d0eda4277
Elior <heliosaian@gmail.com>
2024-04-27 20:41:24 +0300

    wip: icons replacements

---
+++ common/src/main/java/com/mallowigi/icons/replacements/IconReplacements.kt
@@ -0,0 +1,42 @@
+/*
+ * The MIT License (MIT)
+ *
+ *  Copyright (c) 2015-2022 Elior "Mallowigi" Boukhobza
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+package com.mallowigi.icons.replacements
+
+import com.intellij.util.xmlb.annotations.Property
+import com.intellij.util.xmlb.annotations.XCollection
+import com.thoughtworks.xstream.annotations.XStreamAlias
+import java.util.*
+
+@XStreamAlias("root")
+class IconReplacements {
+  @Property
+  @XCollection
+  @XStreamAlias("iconReplacements")
+  val iconReplacements: Set<IconReplacement> = emptySet()
+    get() = Collections.unmodifiableSet(field)
+
+  fun getReplacement(path: String): String? = iconReplacements.find {
+    it.replacePaths.any { replacePath -> replacePath.path == path }
+  }?.icon
+}
