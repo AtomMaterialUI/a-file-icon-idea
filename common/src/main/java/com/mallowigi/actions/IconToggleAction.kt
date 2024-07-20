20d7134f40f41fde2320dc226dd3acee5028d10c
Elior <heliosaian@gmail.com>
2024-07-20 09:36:06 +0300

    small fixes

--- common/src/main/java/com/mallowigi/actions/IconToggleAction.kt
+++ common/src/main/java/com/mallowigi/actions/IconToggleAction.kt
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2023 Elior "Mallowigi" Boukhobza
+ * Copyright (c) 2015-2024 Elior "Mallowigi" Boukhobza
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
 package com.mallowigi.actions
 
@@ -65,7 +64,7 @@ abstract class IconToggleAction : ToggleAction() {
   override fun update(e: AnActionEvent) {
     val selected = isSelected(e)
     val presentation = e.presentation
-    val icon = presentation.icon
+    val icon = presentation.icon ?: return
     Toggleable.setSelected(presentation, selected)
     val fallbackIcon = selectedFallbackIcon()
     val actionButtonIcon = ObjectUtils.notNull(UIManager.getIcon("ActionButton.backgroundIcon"), fallbackIcon)
